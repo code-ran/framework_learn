0765923bef4853e10bad663471cb650b965ab16f
code-ran <rsb18252508590@163.com>
2024-08-26 00:07:06 +0800

    [增加]spring代码提交

---
+++ spring_learn/src/main/java/com/ransibi/Main.java
@@ -0,0 +1,12 @@
+package com.ransibi;
+
+/**
+ * @Author: ransibi
+ * @Date: 2024/08/25/23:28
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
