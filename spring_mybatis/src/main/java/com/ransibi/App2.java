a69337495d2e770df1ea828b38f6eed815638ca4
code-ran <rsb18252508590@163.com>
2024-08-27 23:07:52 +0800

    [增加]spring代码提交

---
+++ spring_mybatis/src/main/java/com/ransibi/App2.java
@@ -0,0 +1,19 @@
+package com.ransibi;
+
+
+import com.ransibi.config.SpringConfig;
+import com.ransibi.domain.Account;
+import com.ransibi.service.AccountService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App2 {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        AccountService accountService = ctx.getBean(AccountService.class);
+
+        Account ac = accountService.findById(1);
+        System.out.println(ac);
+    }
+}
---
+++ spring_mybatis/src/main/java/com/ransibi/service/impl/AccountServiceImpl.java
@@ -0,0 +1,38 @@
+package com.ransibi.service.impl;
+
+;
+import com.ransibi.dao.AccountDao;
+import com.ransibi.domain.Account;
+import com.ransibi.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountDao accountDao;
+
+
+    public void save(Account account) {
+        accountDao.save(account);
+    }
+
+    public void update(Account account) {
+        accountDao.update(account);
+    }
+
+    public void delete(Integer id) {
+        accountDao.delete(id);
+    }
+
+    public Account findById(Integer id) {
+        return accountDao.findById(id);
+    }
+
+    public List<Account> findAll() {
+        return accountDao.findAll();
+    }
+}
